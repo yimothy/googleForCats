78bd152c43e3bbedce2828424bd2642968988c17
Yimothy <tim@Yimothys-MacBook-Pro.local>
2016-11-28 14:19:42 -0500

    Connected server to index.html

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express');
+  response.sendfile('./client/index.html');
 });
 
 app.listen(port, (err) => {
@@ -12,20 +12,3 @@ app.listen(port, (err) => {
   }
   console.log('Server is listening on port ', port);
 })
-
-// const requestHandler = (request, response) => {
-//   console.log(request.url);
-//   response.end('Hello');
-// }
-//
-// const server = http.createServer(requestHandler);
-//
-// server.listen(port, (err) => {
-//   if(err) {
-//     return console.log('Error Occurred: ', err);
-//   }
-//
-//   console.log('Server is listening on port', port);
-// })
-//
-// server.listen(port);
